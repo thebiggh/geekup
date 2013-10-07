a036fb880eeeec77e99e985bdd4bca0e183b4f51
Charles Armitage <code@cjrarmitage.co.uk>
2013-10-07 19:11:26 +0100

    Initial Roman Numeral commit

---
+++ Challenge 1 - Roman Calculator/Roman/Roman/RomanTests.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="RomanTests.cs" company="Red Box Recorders Ltd">
+//     Copyright © Red Box Recorders 2013
+// </copyright>
+// -----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Roman
+{
+    class RomanTests
+    {
+    }
+}
